a7d59f7574f3cfe865c3dd773227fec93cc1518c
Mayar Ahmed <126024409+MayarAhmed3920@users.noreply.github.com>
2024-05-29 15:54:12 +0300

    [88] [Time Beats: 100.00%] [Memory Beats: 71.63%] - LeetPush

---
+++ 88-Merge-Sorted-Array.cpp
@@ -0,0 +1,32 @@
+class Solution {
+public:
+    void merge(vector<int>& nums1, int m, vector<int>& nums2, int n) {
+         int p1=m-1;
+         int p2=n-1;
+         int p=m+n-1;
+        while (p1 >= 0 && p2 >= 0) {
+        if (nums1[p1] > nums2[p2]) {
+            nums1[p] = nums1[p1];
+            p1--;
+        } else {
+            nums1[p] = nums2[p2];
+            p2--;
+        }
+        p--;
+    }
+    
+    while (p2 >= 0) {
+        nums1[p--] = nums2[p2--];
+    }
+
+        for(int i=0;i<m;i++){
+        if(i==0)
+          cout<<"["<<nums1[i]<<",";
+          else if(i==m-1)
+          cout<<nums1[i]<<","<<"]";
+          else
+          cout<<nums1[i]<<",";
+        }
+        
+    }
+};
